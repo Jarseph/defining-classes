21683d982748d15c7d35bd42d58f92e7d0cd1c99
Joseph Spencer <104227087+Jarseph@users.noreply.github.com>
2022-08-25 15:52:46 -0700

    Add files via upload

---
+++ src/student.js
@@ -0,0 +1,12 @@
+// Your code here
+class Student {
+  constructor(firstName, lastName, birthYear, birthMonth, birthDay, gender) {
+    this.firstName = "Bobby",
+    this.lastName = "Smith",
+    this.birthYear = 2001,
+    this.birthMonth = 2,
+    this.birthDay = 3,
+    this.gender = "male"
+  }
+}
+module.exports = Student;
